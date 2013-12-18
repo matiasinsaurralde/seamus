c73e4d3d5d33b4c0516e8654d46dcecb20b05f53
Matias Insaurralde <matiasbaruchinsaurralde@gmail.com>
2013-12-18 04:33:30 -0300

    html para control etc.

--- client/main.go
+++ client/main.go
@@ -1,12 +1,34 @@
 package main
 
-import ( "fmt"
+import ( "net/http"
+         "strconv"
+         "strings"
          "net"
-         "os" )
+         "fmt" )
+
+func controlHandler( w http.ResponseWriter, r *http.Request, conn net.Conn ) {
+  http.ServeFile( w, r, "control.html" )
+  command := strings.Replace( r.URL.Path, "/", "", 1 )
+  switch command {
+    case "accelerate": sendCommand( conn, 0 )
+    case "reverse": sendCommand( conn, 1 )
+    case "left": sendCommand( conn, 2 )
+    case "right": sendCommand( conn, 3 )
+  }
+}
+
+func sendCommand( conn net.Conn, command int ) {
+  fmt.Println("sendCommand!")
+  fmt.Fprintf( conn, strconv.Itoa( command ) )
+}
 
 func main() {
-  fmt.Println("SEAMUS");
-  conn, _ := net.Dial( "tcp", os.Args[1] + ":9000" )
-  fmt.Fprintf( conn, "" )
-  fmt.Fprintf( conn, os.Args[2] )
+
+  conn, _ := net.Dial( "tcp", "192.168.1.200:9000" )
+
+  sendCommand( conn, 0 )
+
+  http.HandleFunc( "/", func( w http.ResponseWriter, r *http.Request) { controlHandler(w, r, conn) })
+  http.ListenAndServe( ":8000", nil )
+
 }
