7e11b278651553d73a081d17b3757ec3de51065a
Matias Insaurralde <matiasbaruchinsaurralde@gmail.com>
2013-12-19 19:25:56 -0300

    changing pin numbers

--- client/main.go
+++ client/main.go
@@ -10,22 +10,20 @@ func controlHandler( w http.ResponseWriter, r *http.Request, conn net.Conn ) {
   http.ServeFile( w, r, "control.html" )
   command := strings.Replace( r.URL.Path, "/", "", 1 )
   switch command {
-    case "accelerate": sendCommand( conn, 0 )
-    case "reverse": sendCommand( conn, 1 )
-    case "left": sendCommand( conn, 2 )
-    case "right": sendCommand( conn, 3 )
+    case "accelerate": sendCommand( conn, 7 )
+    case "reverse": sendCommand( conn, 6 )
+    case "left": sendCommand( conn, 5 )
+    case "right": sendCommand( conn, 4 )
   }
 }
 
 func sendCommand( conn net.Conn, command int ) {
-  fmt.Fprintf( conn, strconv.Itoa( command ) )
+  fmt.Fprintf( conn, strconv.Itoa( command ) + "," + strconv.Itoa( 1000 ) + "\n" )
 }
 
 func main() {
 
   conn, _ := net.Dial( "tcp", "192.168.1.200:9000" )
-
-  sendCommand( conn, 0 )
 
   http.HandleFunc( "/", func( w http.ResponseWriter, r *http.Request) { controlHandler(w, r, conn) })
   http.ListenAndServe( ":8000", nil )
